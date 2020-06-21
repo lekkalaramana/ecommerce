377ab9cb2d8928e3cca656fa6c7ec444479d0632
Lekkala Ramana <lekkala.ramanaiah@weinvest.net>
2020-06-21 13:19:51 +0530

    changes added

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_055345) do
+ActiveRecord::Schema.define(version: 2020_06_21_042211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -28,10 +22,8 @@ ActiveRecord::Schema.define(version: 2020_06_21_055345) do
     t.text "description"
     t.string "size", array: true
     t.text "colour", array: true
-    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
